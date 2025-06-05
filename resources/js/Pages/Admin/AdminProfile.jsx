5aaa51d93a3151b8b7d9aab96045eadcfa6ceb9f
KhantYadanarMoe <khantyadanarmoe113@gmail.com>
2025-06-05 16:45:50 +0630

    MoteSine

--- resources/js/Pages/Admin/AdminProfile.jsx
+++ resources/js/Pages/Admin/AdminProfile.jsx
@@ -2,8 +2,114 @@ import Profile from "../../../images/profile.jpg";
 import { Mail, Phone, BadgeCheck } from "lucide-react";
 import { Input } from "../../Components/ui/input";
 import { motion } from "framer-motion";
+import { useAuth } from "@/contexts/AuthContext";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function AdminProfile() {
+    const { user, setUser } = useAuth();
+    const [image, setImage] = useState(null);
+    // prepare state to store form data
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        address: "",
+    });
+    // store errors state
+    const [errors, setErrors] = useState({});
+
+    // prepare to move another route/page after sending data
+    const navigate = useNavigate();
+
+    // Handle HTML inputs
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setForm((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    // Handle image input
+    const uploadImg = (e) => {
+        const file = e.target.files[0];
+        if (file && file.type.startsWith("image/")) {
+            setImage(file);
+        }
+    };
+
+    useEffect(() => {
+        if (user) {
+            setForm({
+                name: user.name || "",
+                email: user.email || "",
+                phone: user.phone || "",
+                address: user.address || "",
+            });
+        }
+    }, [user]);
+
+    // form submit function
+    const submit = async (e) => {
+        e.preventDefault();
+
+        // url and method to use in sending data using axios
+        let url = "/api/user/" + user.id;
+        let method = "post";
+
+        // create new object to store form data to send
+        let formData = new FormData();
+
+        console.log("Form Data before submitting:", form);
+
+        // store state data in object
+        formData.append("name", form.name);
+        formData.append("email", form.email);
+        formData.append("phone", form.phone);
+        formData.append("address", form.address);
+
+        console.log("Form data after appending:", formData);
+
+        if (image) {
+            formData.append("image", image);
+        }
+
+        formData.append("_method", "PUT");
+
+        try {
+            const csrfToken = document
+                .querySelector('meta[name="csrf-token"]')
+                .getAttribute("content");
+
+            // send data
+            const res = await axios[method](url, formData, {
+                headers: {
+                    "X-CSRF-TOKEN": csrfToken,
+                    "Content-Type": "multipart/form-data",
+                },
+            });
+
+            // success condition
+            if (res.data.message === "User updated successfully.") {
+                const updatedUser = res.data.user;
+
+                setUser(updatedUser);
+
+                setImage(null);
+            }
+        } catch (error) {
+            console.error("Error updating user:", error);
+
+            // failed condition
+            if (error.response && error.response.status === 422) {
+                setErrors(error.response.data.errors);
+            }
+        }
+    };
+
     return (
         <motion.div
             initial={{ visibility: "hidden", opacity: 0 }}
@@ -18,54 +124,64 @@ export default function AdminProfile() {
                     <form action="" className="mt-6">
                         <div className="flex gap-6 items-center mb-7">
                             <img
-                                src={Profile}
-                                alt="profile"
+                                src={
+                                    image
+                                        ? URL.createObjectURL(image)
+                                        : user?.image
+                                        ? `/storage/${user.image}`
+                                        : Profile
+                                }
+                                alt="User Profile"
                                 className="w-20 h-20 md:w-24 md:h-24 object-cover rounded-full border-2 border-accentRed"
                             />
                             <div>
-                                <h1 className="font-medium">
-                                    Khant Yadanar Moe
-                                </h1>
+                                <h1 className="font-medium">{user?.name}</h1>
                                 <p className="text-gray-800 text-sm">
-                                    NYC, USA
+                                    {user?.address}
                                 </p>
                                 <div className="flex gap-2 mt-2">
-                                    <button className="px-2 py-2 text-sm bg-accentGreen hover:bg-hoverGreen duration-300 text-white rounded-md">
-                                        Upload new
-                                    </button>
+                                    <Input
+                                        id="image-upload"
+                                        name="image"
+                                        type="file"
+                                        accept="image/*"
+                                        onChange={uploadImg}
+                                        className="hidden"
+                                    />
+                                    <label
+                                        htmlFor="image-upload"
+                                        className="inline-block rounded-lg border border-accentGreen text-accentGreen hover:border-hoverGreen hover:text-hoverGreen hover:bg-gray-100 font-medium duration-300 px-3 py-1 cursor-pointer"
+                                    >
+                                        Upload New
+                                    </label>
                                     <button className="px-2 py-2 text-sm bg-accentRed hover:bg-accentRed duration-300 text-white rounded-md">
                                         Delete
                                     </button>
                                 </div>
                             </div>
                         </div>
-                        <div className="md:flex gap-3">
-                            <div className="md:w-1/2 mb-4">
-                                <label htmlFor="firstName">First Name</label>
-                                <Input
-                                    id="firstName"
-                                    type="text"
-                                    placeholder="Enter your first name"
-                                    className="mt-1 border-gray-500"
-                                />
-                            </div>
-                            <div className="md:w-1/2 mb-4">
-                                <label htmlFor="lastName">Last Name</label>
-                                <Input
-                                    id="lastName"
-                                    type="text"
-                                    placeholder="Enter your last name"
-                                    className="mt-1 border-gray-500"
-                                />
-                            </div>
+                        <div className="mb-4">
+                            <label htmlFor="name">Name</label>
+                            <Input
+                                id="name"
+                                name="name"
+                                type="text"
+                                placeholder="Enter your name"
+                                value={form.name}
+                                onChange={handleInputChange}
+                                className="mt-1 border-gray-500"
+                            />
                         </div>
                         <div className="md:flex gap-3">
                             <div className="md:w-1/2 mb-4">
                                 <label htmlFor="email">Email</label>
                                 <Input
                                     id="email"
+                                    name="email"
                                     type="text"
                                     placeholder="Enter your email"
+                                    value={form.email}
+                                    onChange={handleInputChange}
                                     className="mt-1 border-gray-500"
                                 />
                             </div>
@@ -73,8 +189,11 @@ export default function AdminProfile() {
                                 <label htmlFor="phone">Phone</label>
                                 <Input
                                     id="phone"
+                                    name="phone"
                                     type="text"
                                     placeholder="Enter your phone"
+                                    value={form.phone}
+                                    onChange={handleInputChange}
                                     className="mt-1 border-gray-500"
                                 />
                             </div>
@@ -82,13 +201,20 @@ export default function AdminProfile() {
                         <div className="mb-4">
                             <label htmlFor="address">Address</label>
                             <textarea
+                                id="address"
+                                name="address"
                                 type="text"
                                 placeholder="Enter your Address"
+                                value={form.address}
+                                onChange={handleInputChange}
                                 className="w-full mt-1 px-3 py-2 border border-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-crimson focus:border-crimson text-gray-700"
                             ></textarea>
                         </div>
                         <div className="flex justify-end gap-2">
-                            <button className="px-2 py-2 text-sm bg-accentGreen hover:bg-hoverGreen duration-300 text-white rounded-md">
+                            <button
+                                className="px-2 py-2 text-sm bg-accentGreen hover:bg-hoverGreen duration-300 text-white rounded-md"
+                                onClick={submit}
+                            >
                                 Submit
                             </button>
                             <button className="px-2 py-2 text-sm bg-accentRed hover:bg-accentRed duration-300 text-white rounded-md">
@@ -144,12 +270,18 @@ export default function AdminProfile() {
                 <div className="mt-3 px-4 py-3 bg-white shadow-md rounded-lg">
                     <h1 className="text-lg font-medium">Your Account</h1>
                     <img
-                        src={Profile}
+                        src={
+                            image
+                                ? URL.createObjectURL(image)
+                                : user?.image
+                                ? `/storage/${user.image}`
+                                : Profile
+                        }
                         alt="profile"
                         className="mt-5 w-24 h-24 object-cover mx-auto rounded-full border border-accentRed"
                     />
                     <h1 className="text-center font-medium mt-1">
-                        Khant Yadanar Moe
+                        {user?.name}
                     </h1>
                     <p className="text-center text-gray-800 text-sm">Admin</p>
                     <hr className="border-t-gray-400 my-3" />
@@ -161,16 +293,14 @@ export default function AdminProfile() {
                             <Mail size={16} className="text-accentRed" />
                             Email
                         </p>
-                        <p className="text-sm text-gray-800">
-                            khantyadanarmoe487@gmail.com
-                        </p>
+                        <p className="text-sm text-gray-800">{user?.email}</p>
                     </div>
                     <div className="my-3">
                         <p className="text-sm flex gap-1 items-center mb-1">
                             <Phone size={16} className="text-accentRed" />
                             Phone
                         </p>
-                        <p className="text-sm text-gray-800">(376) 457 732</p>
+                        <p className="text-sm text-gray-800">{user?.phone}</p>
                     </div>
 
                     <div className="my-3">
