4ff619f5d2c189bab3a1a23091c512b454d032f3
KhantYadanarMoe <khantyadanarmoe113@gmail.com>
2025-06-24 10:31:33 +0630

    MoteSine

--- resources/views/emails/contact_reply.blade.php
+++ resources/views/emails/contact_reply.blade.php
@@ -1,5 +1,14 @@
-<p>Hi,</p>
+<div class="max-w-xl mx-auto p-6 bg-white text-gray-800 rounded-lg shadow font-sans border border-gray-200">
+  <p class="text-base mb-4">Hi,</p>
 
-<p>{{ $messageBody }}</p>
+  <p class="text-base mb-4">Thank you for reaching out to us. We’ve received your message and appreciate you taking the time to contact us.</p>
 
-<p>Best regards,<br>Your Admin</p>
+  <p class="text-base mb-4">{{ $messageBody }}</p>
+
+  <p class="text-base mb-4">If you have any further questions or need additional help, feel free to reply to this message. We're here to help!</p>
+
+  <p class="text-base">Best regards,<br><span class="font-semibold">Mote Sine.</span></p>
+
+  <hr class="my-6 border-gray-300">
+  <p class="text-xs text-gray-500">This response was generated based on your inquiry submitted via our contact form.</p>
+</div>
